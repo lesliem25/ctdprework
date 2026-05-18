28bb17aef344c65febd644d9628fb86776a7127e
lesliem25 <lizmvelazquez14@gmail.com>
2026-05-17 23:26:25 -0400

    updated draft 1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Code the Dream Prework
+# Code the Dream Prework by Leslie Mendex
 
 ## Introduction
 
-# This repo has useful practice with Git and Github and almost no actual contents :D
+# This website allows users who want to know what the temperature in the U.S would be like. Every time the user clicks on the button, they are given the temperature one more hour in advance. The website only allows users to see the temperature predictions for up to 12 hours in the future and the temperature is in Celsius. 
--- prework.js
+++ prework.js
@@ -1,9 +1,33 @@
-fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
-    .then(response => {
+
+var hour = 0;
+
+// fetching Data from the NOAA U.S. Weather Forecast
+async function fetchData(){
+    try{
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&models=gfs_seamless');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+        //Getting the Temperature Information
+        const data = await response.json();
+        const allTemps = data.hourly.temperature_2m;
+        const hourlyTemp = Object.fromEntries(Object.entries(allTemps).slice(0,13));
+        //Getting the Weather Information
+        const allTimes = data.hourly.time;
+        var tempAtHour = Object.values(hourlyTemp)[hour];
+        const times = Object.fromEntries(Object.entries(allTimes).slice(0,13));
+        var specificTime = Object.values(times)[hour];
+        $("#date").text("Date: " + specificTime.slice(5,10));
+        //Ensuring the website does not display information as soon as it shows the prediction for the 12th hour
+        if (hour < 12){
+            $("#temp").append(" " + Number(tempAtHour)+1);
+            $("#hourlyTemp").append(" " + specificTime.slice(11,17));
+            hour +=1;
+        }
+    }
+    catch(error){
+        console.error('Error:', error);
+    }
+}
+
+$("#fetchTemp").on("click", fetchData);
